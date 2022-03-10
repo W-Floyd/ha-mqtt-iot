b9d248b3c1d6f4c554f8a352f8c7d7643c76d0b7
William Floyd <william.png2000@gmail.com>
2022-03-10 10:34:10 -0600

    help: Delete old.

--- helpers/generators/snippets/DeviceName.go
+++
@@ -1,8 +0,0 @@
-if common.InstanceName == "" {
-	if unchanged {
-		logging.LogError("Unable to generate config.ConfigurationTHIS, missing common.InstanceName")
-	}
-} else {
-	n += 1
-	config.Configuration.Device.Name = common.StringPointer(common.InstanceName)
-}
