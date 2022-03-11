bc5659587ec23f90979539f20f4fcdf514cd8f60
William Floyd <william.png2000@gmail.com>
2022-03-11 14:36:29 -0600

    hadiscovery: Refactor.

--- hadiscovery/hadiscovery.go
+++ hadiscovery/hadiscovery.go
@@ -70,6 +70,24 @@ type store struct {
 	Switch map[string]string
 }
 
+func initStore() store {
+	var s store
+	s.BinarySensor = make(map[string]string)
+	s.Light.BrightnessState = make(map[string]string)
+	s.Light.ColorTempState = make(map[string]string)
+	s.Light.EffectState = make(map[string]string)
+	s.Light.HsState = make(map[string]string)
+	s.Light.RgbState = make(map[string]string)
+	s.Light.State = make(map[string]string)
+	s.Light.WhiteValueState = make(map[string]string)
+	s.Light.XyState = make(map[string]string)
+	s.Sensor = make(map[string]string)
+	s.Switch = make(map[string]string)
+	return s
+}
+
+var stateStore = initStore()
+
 // UpdateState publishes any new state for a device
 // This is for a light
 // func (device Light) UpdateState(client mqtt.Client) {
@@ -287,22 +305,3 @@ type store struct {
 // }
 
 ///////////////////
-
-func initStore() store {
-	var s store
-	s.BinarySensor = make(map[string]string)
-	s.Light.BrightnessState = make(map[string]string)
-	s.Light.ColorTempState = make(map[string]string)
-	s.Light.EffectState = make(map[string]string)
-	s.Light.HsState = make(map[string]string)
-	s.Light.RgbState = make(map[string]string)
-	s.Light.State = make(map[string]string)
-	s.Light.WhiteValueState = make(map[string]string)
-	s.Light.XyState = make(map[string]string)
-	s.Sensor = make(map[string]string)
-	s.Switch = make(map[string]string)
-	return s
-}
-
-var topicStore = make(map[string]*func(mqtt.Message, mqtt.Client))
-var stateStore = initStore()
