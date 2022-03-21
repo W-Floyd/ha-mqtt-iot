54d2e959af97b0903e58a38f25780ced9a88a351
William Floyd <william.png2000@gmail.com>
2022-03-21 09:50:35 -0500

    scripts: Fix user determination when ssh'ed in.

--- scripts/run-in-user-session.sh
+++ scripts/run-in-user-session.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+set -x
+
 function run-in-user-session() {
     _display_id=":$(find /tmp/.X11-unix/* | sed 's#/tmp/.X11-unix/X##' | head -n 1)"
-    _username=$(who | grep "\(${_display_id}\)" | awk '{print $1}')
+    _username=$(who | grep "\(${_display_id}\)" | awk '{print $1}' | sort | uniq)
     _user_id=$(id -u "${_username}")
     _environment=("DISPLAY=$_display_id" "DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/${_user_id}/bus")
     sudo -Hu "${_username}" env "${_environment[@]}" "${@}"
