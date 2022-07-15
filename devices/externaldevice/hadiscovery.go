16961993329db4671410fd5ce367f2fc9c8e0d25
William Floyd <william.png2000@gmail.com>
2022-07-14 22:05:01 -0400

    misc: Update version number and sample config.

--- devices/externaldevice/hadiscovery.go
+++ devices/externaldevice/hadiscovery.go
@@ -6,7 +6,7 @@ const DiscoveryPrefix = "homeassistant"
 
 // SWVersion is the software version.
 // TODO - Move this elsewhere maybe?
-var SWVersion = "0.4.5"
+var SWVersion = "0.5.0"
 
 var SoftwareName = "Homeassistant MQTT IOT"
 
