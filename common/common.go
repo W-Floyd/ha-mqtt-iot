2ae6fa60a3dcb8b31ad80397775c8814de35d7b5
William Floyd <william.png2000@gmail.com>
2022-07-18 20:52:36 -0400

    fix: Logging levels in the config actually take effect.

--- common/common.go
+++ common/common.go
@@ -6,11 +6,24 @@ import (
 	"time"
 )
 
+type LogLevels struct {
+	Debug    bool
+	Error    bool
+	Warn     bool
+	Critical bool
+}
+
 var (
 	Retain           bool          = false
 	QoS              byte          = 0
 	HADiscoveryDelay time.Duration = 500 * time.Millisecond
 	MachineID        string
+	LogState         = LogLevels{
+		Debug:    false,
+		Error:    false,
+		Warn:     false,
+		Critical: false,
+	}
 )
 
 var DebugLog = log.New(os.Stdout, "DEBUG   ", 0)
@@ -21,16 +34,30 @@ var CriticalLog = log.New(os.Stdout, "CRITICAL", 0)
 const logPrefix = "[ha-mqtt]  "
 
 func LogError(message ...interface{}) {
-	if len(message) > 1 {
-		for _, mes := range message[:len(message)-2] {
-			ErrorLog.Printf("%v%v\n", logPrefix, mes)
+	if LogState.Error {
+		if len(message) > 1 {
+			for _, mes := range message[:len(message)-2] {
+				ErrorLog.Printf("%v%v\n", logPrefix, mes)
+			}
 		}
+		ErrorLog.Fatalf("%v%v\n", logPrefix, message[len(message)-1])
+	} else {
+		os.Exit(1)
 	}
-	ErrorLog.Fatalf("%v%v\n", logPrefix, message[len(message)-1])
 }
 
 func LogDebug(message ...interface{}) {
-	for _, mes := range message {
-		DebugLog.Printf("%v%v\n", logPrefix, mes)
+	if LogState.Debug {
+		for _, mes := range message {
+			DebugLog.Printf("%v%v\n", logPrefix, mes)
+		}
+	}
+}
+
+func LogWarning(message ...interface{}) {
+	if LogState.Warn {
+		for _, mes := range message {
+			WarnLog.Printf("%v%v\n", logPrefix, mes)
+		}
 	}
 }
